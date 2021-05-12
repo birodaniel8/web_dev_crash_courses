58938a089e23561cf21b634bfec80f823eebdeb3
birodaniel8 <birodaniel8@gmail.com>
2021-05-12 20:33:55 +0200

    django auth

--- react_django_crash_course/leadmanager/frontend/src/actions/leads.js
+++ react_django_crash_course/leadmanager/frontend/src/actions/leads.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { createMessage } from "./messages";
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS} from "./types";
+import { createMessage, returnErrors } from "./messages";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
 // Get leads:
-export const getLeads = () => dispatch => {
+export const getLeads = () => (dispatch) => {
   axios
     .get("/api/leads/")
     .then((response) => {
@@ -11,40 +11,38 @@ export const getLeads = () => dispatch => {
         type: GET_LEADS,
         payload: response.data,
       });
-    }).catch((error) => console.log(error));
+    })
+    .catch((error) => {
+      dispatch(returnErrors(error.response.data, error.response.status));
+    });
 };
 
 // Delete lead:
-export const deleteLead = (id) => dispatch => {
+export const deleteLead = (id) => (dispatch) => {
   axios
     .delete(`/api/leads/${id}/`)
     .then((response) => {
-      dispatch(createMessage({ deleteLead: "Lead Deleted"}));
+      dispatch(createMessage({ deleteLead: "Lead Deleted" }));
       dispatch({
         type: DELETE_LEAD,
         payload: id,
       });
-    }).catch((error) => console.log(error));
+    })
+    .catch((error) => console.log(error));
 };
 
 // Add lead:
-export const addLead = (lead) => dispatch => {
+export const addLead = (lead) => (dispatch) => {
   axios
     .post("/api/leads/", lead)
     .then((response) => {
-      dispatch(createMessage({ addLead: "Lead Added"}));
+      dispatch(createMessage({ addLead: "Lead Added" }));
       dispatch({
         type: ADD_LEAD,
         payload: response.data,
       });
-    }).catch((error) => {
-      const errors = {
-        msg: error.response.data,
-        status: error.response.status,
-      }
-      dispatch({
-        type: GET_ERRORS,
-        payload: errors
-      });
+    })
+    .catch((error) => {
+      dispatch(returnErrors(error.response.data, error.response.status));
     });
 };
